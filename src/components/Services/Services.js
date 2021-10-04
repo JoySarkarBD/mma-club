2864fcb79367822f5a3dcb0318dcbaf82d6af013
JOY_SARKAR <joysarkarbd407@gmail.com>
2021-10-04 23:25:00 +0600

    small modification

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -13,7 +13,7 @@ const Services = () => {
                         <img src="https://demo-images.netlify.app/teach/1.png" className="card-img-top w-25 mx-auto" alt="..." />
                         <div className="card-body">
                             <h2 className="card-title text-center">24/7 <span className="text-danger ">Support</span></h2>
-                            <p className="card-text">Get practical lectures and classes recorded as videos. This will create a better storage of materials for the future. And you can practice at home with these materials.</p>
+                            <p className="card-text">24/7 Online Support - We always listen to our students. Our team is always there to help them whenever they have a problem.</p>
                         </div>
                     </div>
                 </div>
@@ -23,7 +23,9 @@ const Services = () => {
                         <img src="https://demo-images.netlify.app/teach/4.png" className="card-img-top w-25 mx-auto" alt="..." />
                         <div className="card-body">
                             <h2 className="card-title text-center pb-4">Class <span className="text-danger ">Video</span></h2>
-                            <p className="card-text">24/7 Online Support - We always listen to our students. Our team is always there to help them whenever they have a problem.</p>
+                            <p className="card-text">
+                                Get practical lectures and classes recorded as videos. This will create a better storage of materials for the future. And you can practice at home with these materials.
+                            </p>
                         </div>
                     </div>
                 </div>
